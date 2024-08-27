9c088b11b0548b6c78c8303e33e28e3a9c06f947
Gabriel Rodriguez <gsrdz@tutanota.com>
2024-08-27 12:21:05 -0400

    Changed X handle

--- src/app/_components/socials.tsx
+++ src/app/_components/socials.tsx
@@ -1,7 +1,7 @@
 export default function Socials() {
   return (
     <div className="flex place-content-center gap-14">
-      <a href="https://x.com/Gabriel1857749" target="_blank">
+      <a href="https://x.com/gsevendev" target="_blank">
         <img src="/icons/x.svg" alt="X" className="w-10 h-10" />
       </a>
       <a href="https://mastodon.social/@g7dev" target="_blank">
